edcba0bddef5b570774bcdcd7dc0a6fbf215da1c
hqleeUstc <hqleee@outlook.com>
2022-10-18 23:08:02 +0800

    合并icnn tensorflow的代码

--- src/icnn.py
+++ src/icnn.py
@@ -85,7 +85,119 @@ class OriginalICNN(nn.Module):
         self.droput = dropout
         self.activation = activation
         self.rank = rank
+        self.lrelu = 0.01
+        self.U_layer_sizes = [32, 32, 32]
+        self.Z_layer_sizes = [32, 32, 32]
+        self.nlayers = 2
+        self.dimX = 32
+        self.dimY = 32
+        self.dimZ = 32
         
+        sizes = zip(hidden_layer_sizes[:-1], hidden_layer_sizes[1:])
+        # U layer
+        self.u_first_fc = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(in_features, out_features, bias=False),
+                nn.ReLU(True),
+                # nn.BatchNorm(out_features),
+                nn.Dropout(dropout)
+            )
+            for (in_features, out_features) in sizes
+        ])
+
+        self.u_second_fc = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(in_features, out_features, bias=False),
+                # nn.ReLU(True),
+                # nn.BatchNorm(out_features),
+                nn.Dropout(dropout)
+            )
+            for (in_features, out_features) in sizes
+        ])
+
+        # ZU part for Z layer
+        self.z_zu_first_fc = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(in_features, out_features, bias=False),
+                # nn.ReLU(True),
+                # nn.BatchNorm(out_features),
+                nn.Dropout(dropout)
+            )
+            for (in_features, out_features) in sizes
+        ])
+
+        self.z_zu_second_fc = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(in_features, out_features, bias=False),
+                # nn.ReLU(True),
+                # nn.BatchNorm(out_features),
+                nn.Dropout(dropout)
+            )
+            for (in_features, out_features) in sizes
+        ])
+
+        # YU part for Z layer
+        self.z_yu_first_fc = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(in_features, out_features, bias=False),
+                # nn.ReLU(True),
+                # nn.BatchNorm(out_features),
+                nn.Dropout(dropout)
+            )
+            for (in_features, out_features) in sizes
+        ])
+
+        self.z_yu_second_fc = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(in_features, out_features, bias=False),
+                # nn.ReLU(True),
+                # nn.BatchNorm(out_features),
+                nn.Dropout(dropout)
+            )
+            for (in_features, out_features) in sizes
+        ])
+
+        # self.z_yu_third_fc = nn.ModuleList([
+        #     nn.Sequential(
+        #         nn.Linear(in_features, out_features, bias=False),
+        #         # nn.ReLU(True),
+        #         # nn.BatchNorm(out_features),
+        #         nn.Dropout(dropout)
+        #     )
+        #     for (in_features, out_features) in sizes
+        # ])
+
+        # U part for Z layer
+        self.z_u_first_fc = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(in_features, out_features, bias=False),
+                # nn.ReLU(True),
+                # nn.BatchNorm(out_features),
+                nn.Dropout(dropout)
+            )
+            for (in_features, out_features) in sizes
+        ])
+
+        # self.z_u_second_fc = nn.ModuleList([
+        #     nn.Sequential(
+        #         nn.Linear(in_features, out_features, bias=False),
+        #         # nn.ReLU(True),
+        #         # nn.BatchNorm(out_features),
+        #         nn.Dropout(dropout)
+        #     )
+        #     for (in_features, out_features) in sizes
+        # ])
+
+        # self.z_u_third_fc = nn.ModuleList([
+        #     nn.Sequential(
+        #         nn.Linear(in_features, out_features, bias=False),
+        #         # nn.ReLU(True),
+        #         # nn.BatchNorm(out_features),
+        #         nn.Dropout(dropout)
+        #     )
+        #     for (in_features, out_features) in sizes
+        # ])
+
         self.quadratic_layers = nn.ModuleList([
             nn.Sequential(
                 ConvexQuadratic(in_dim, out_features, rank=rank, bias=True),
@@ -95,28 +207,65 @@ class OriginalICNN(nn.Module):
         ])
         
         sizes = zip(hidden_layer_sizes[:-1], hidden_layer_sizes[1:])
-        self.convex_layers = nn.ModuleList([
-            nn.Sequential(
-                nn.Linear(in_features, out_features, bias=False),
-                nn.Dropout(dropout)
-            )
-            for (in_features, out_features) in sizes
-        ])
+        # self.convex_layers = nn.ModuleList([
+        #     nn.Sequential(
+        #         nn.Linear(in_features, out_features, bias=False),
+        #         nn.Dropout(dropout)
+        #     )
+        #     for (in_features, out_features) in sizes
+        # ])
         
+        self.lrelu_layer = nn.LeakyReLU(self.lrelu, inplace=True)
         self.final_layer = nn.Linear(hidden_layer_sizes[-1], 1, bias=False)
 
-    def forward(self, input):
-        output = self.quadratic_layers[0](input)
-        for quadratic_layer, convex_layer in zip(self.quadratic_layers[1:], self.convex_layers):
-            output = convex_layer(output) + quadratic_layer(input)
-            if self.activation == 'celu':
-                output = torch.celu(output)
-            elif self.activation == 'softplus':
-                output = F.softplus(output)
-            else:
-                raise Exception('Activation is not specified or unknown.')
+    def forward(self, inputx, inputy):
+        # output = self.quadratic_layers[0](input)
+        prevU = inputx
+        # prevU2 = inputx
+        prevZ = inputy
+        szs = [200, 200]
+        i = 0    
+        for U_layer, y_layer in zip(self.u_first_fc[1:], self.z_yu_first_fc):
+            sz = szs[i] if i < self.nlayers else 1
+            
+            
+                # nn.BatchNorm(out_features)
+
+            yu_u = y_layer(prevU)
+            linear_yu_u = nn.Linear(self.dimX, self.dimY)
+            z_yu = linear_yu_u(torch.matmul(inputy, yu_u), sz)
+
+            linear_z_u = nn.Linear(self.dimX, sz)
+            z_u = linear_z_u(prevU)
+            z = torch.add(z_yu, z_u)    
+
+            if i > 0:
+            
+                zu_u = z_layer(prevU)
+                linear_z_zu = nn.Linear(self.dimX, self.dimZ)
+                z_zu = linear_z_zu(torch.matmul(prevZ, zu_u), sz)
+                z = torch.add(z, z_zu)
+            
+            prevU = U_layer(prevU)
+            if i < nlayers - 1:
+                nn.ReLU(True)
+
+            prevZ = z
+
+            i++
+        z = z.reshape(1, -1)
+        return z
+
+        # for quadratic_layer, convex_layer in zip(self.quadratic_layers[1:], self.convex_layers):
+        #     output = convex_layer(output) + quadratic_layer(input)
+        #     if self.activation == 'celu':
+        #         output = torch.celu(output)
+        #     elif self.activation == 'softplus':
+        #         output = F.softplus(output)
+        #     else:
+        #         raise Exception('Activation is not specified or unknown.')
         
-        return self.final_layer(output) + .5 * self.strong_convexity * (input ** 2).sum(dim=1).reshape(-1, 1)
+        # return self.final_layer(output) + .5 * self.strong_convexity * (input ** 2).sum(dim=1).reshape(-1, 1)
     
     def push(self, input):
         output = autograd.grad(
